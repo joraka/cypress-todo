51789c04ce765eb406e44e001df0d095b3526e22
Jonas <92853498+joraka@users.noreply.github.com>
2025-06-26 23:49:56 +0300

    cypress tests and gh action added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe("todo list", () => {
+  it("header should be displayed", () => {
+    cy.visit("https://todolist.james.am/#/");
+    cy.get("header").contains("To Do List").should("be.visible");
+  });
+  
+  it("footer displays 'Double-click to edit a toodo'", () => {
+    cy.visit("https://todolist.james.am/#/");
+    cy.get("footer").contains("Double-click to edit a toodo").should("be.visible");
+  });
+});
