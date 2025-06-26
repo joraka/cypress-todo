71a64c7dd41fe9ade3d72dd04341f85ba7e795e1
Jonas <92853498+joraka@users.noreply.github.com>
2025-06-27 00:24:26 +0300

    add placeholder test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,7 +5,7 @@ describe("todo list", () => {
     cy.visit("https://todolist.james.am/#/");
     cy.get("header").contains("To Do List").should("be.visible");
   });
-  
+
   it("footer displays 'Double-click to edit a toodo'", () => {
     cy.visit("https://todolist.james.am/#/");
     cy.get("footer").contains("Double-click to edit a toodo").should("be.visible");
@@ -14,5 +14,12 @@ describe("todo list", () => {
   it("footer displays 'Double-click to edit a todo'", () => {
     cy.visit("https://todolist.james.am/#/");
     cy.get("footer").contains("Double-click to edit a todo").should("be.visible");
+  });
+
+  it("todo input placeholder shows 'What need's to be done?'", () => {
+    cy.visit("https://todolist.james.am/#/");
+    cy.get('input[ng-model="newTodo"]')
+      .should("be.visible")
+      .and("have.attr", "placeholder", "What need's to be done?");
   });
 });
